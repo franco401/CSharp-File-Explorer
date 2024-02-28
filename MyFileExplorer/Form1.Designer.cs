80ef159c1c4aca42f8c043f37f3d0577fb80d590
franco401 <francojimenez168@gmail.com>
2024-02-27 21:08:28 -0500

    Implemented a stack for directory traversal
    Users can go back and forth between directories thanks to using a stack.

--- MyFileExplorer/Form1.Designer.cs
+++ MyFileExplorer/Form1.Designer.cs
@@ -28,59 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.FilesTable = new System.Windows.Forms.DataGridView();
-            this.DirName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.FileType = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.DirSize = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.CurrentDir = new System.Windows.Forms.Label();
+            this.PreviousDirBtn = new System.Windows.Forms.Button();
+            this.FilesTable = new System.Windows.Forms.DataGridView();
+            this.DirName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DirType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FileSize = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DateCreated = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NextDirBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.FilesTable)).BeginInit();
             this.SuspendLayout();
-            // 
-            // FilesTable
-            // 
-            this.FilesTable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.FilesTable.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.FilesTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.FilesTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.DirName,
-            this.FileType,
-            this.DirSize});
-            this.FilesTable.GridColor = System.Drawing.SystemColors.ButtonFace;
-            this.FilesTable.Location = new System.Drawing.Point(3, 40);
-            this.FilesTable.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.FilesTable.Name = "FilesTable";
-            this.FilesTable.RowHeadersWidth = 51;
-            this.FilesTable.RowTemplate.Height = 29;
-            this.FilesTable.Size = new System.Drawing.Size(797, 316);
-            this.FilesTable.TabIndex = 2;
-            this.FilesTable.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.FilesTable_CellMouseDoubleClick);
-            // 
-            // DirName
-            // 
-            this.DirName.HeaderText = "Name";
-            this.DirName.MinimumWidth = 300;
-            this.DirName.Name = "DirName";
-            this.DirName.ReadOnly = true;
-            this.DirName.Width = 300;
-            // 
-            // FileType
-            // 
-            this.FileType.HeaderText = "Type";
-            this.FileType.MinimumWidth = 200;
-            this.FileType.Name = "FileType";
-            this.FileType.ReadOnly = true;
-            this.FileType.Width = 200;
-            // 
-            // DirSize
-            // 
-            this.DirSize.HeaderText = "Size";
-            this.DirSize.MinimumWidth = 200;
-            this.DirSize.Name = "DirSize";
-            this.DirSize.ReadOnly = true;
-            this.DirSize.Width = 200;
             // 
             // label1
             // 
@@ -94,17 +52,95 @@
             // CurrentDir
             // 
             this.CurrentDir.AutoSize = true;
-            this.CurrentDir.Location = new System.Drawing.Point(125, 7);
+            this.CurrentDir.Location = new System.Drawing.Point(130, 7);
             this.CurrentDir.Name = "CurrentDir";
             this.CurrentDir.Size = new System.Drawing.Size(98, 15);
             this.CurrentDir.TabIndex = 4;
             this.CurrentDir.Text = "Current Directory";
+            // 
+            // PreviousDirBtn
+            // 
+            this.PreviousDirBtn.Location = new System.Drawing.Point(10, 23);
+            this.PreviousDirBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PreviousDirBtn.Name = "PreviousDirBtn";
+            this.PreviousDirBtn.Size = new System.Drawing.Size(108, 22);
+            this.PreviousDirBtn.TabIndex = 7;
+            this.PreviousDirBtn.Text = "Back";
+            this.PreviousDirBtn.UseVisualStyleBackColor = true;
+            this.PreviousDirBtn.Click += new System.EventHandler(this.PreviousDirBtn_Click);
+            // 
+            // FilesTable
+            // 
+            this.FilesTable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FilesTable.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.FilesTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.FilesTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DirName,
+            this.DirType,
+            this.FileSize,
+            this.DateCreated});
+            this.FilesTable.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.FilesTable.Location = new System.Drawing.Point(10, 50);
+            this.FilesTable.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.FilesTable.Name = "FilesTable";
+            this.FilesTable.RowHeadersWidth = 51;
+            this.FilesTable.RowTemplate.Height = 29;
+            this.FilesTable.Size = new System.Drawing.Size(788, 306);
+            this.FilesTable.TabIndex = 2;
+            this.FilesTable.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.FilesTable_CellMouseDoubleClick);
+            // 
+            // DirName
+            // 
+            this.DirName.HeaderText = "Name";
+            this.DirName.MinimumWidth = 6;
+            this.DirName.Name = "DirName";
+            this.DirName.ReadOnly = true;
+            this.DirName.Width = 256;
+            // 
+            // DirType
+            // 
+            this.DirType.HeaderText = "Type";
+            this.DirType.MinimumWidth = 6;
+            this.DirType.Name = "DirType";
+            this.DirType.ReadOnly = true;
+            this.DirType.Width = 125;
+            // 
+            // FileSize
+            // 
+            this.FileSize.HeaderText = "Size";
+            this.FileSize.MinimumWidth = 6;
+            this.FileSize.Name = "FileSize";
+            this.FileSize.ReadOnly = true;
+            this.FileSize.Width = 125;
+            // 
+            // DateCreated
+            // 
+            this.DateCreated.HeaderText = "Date Created";
+            this.DateCreated.MinimumWidth = 6;
+            this.DateCreated.Name = "DateCreated";
+            this.DateCreated.ReadOnly = true;
+            this.DateCreated.Width = 256;
+            // 
+            // NextDirBtn
+            // 
+            this.NextDirBtn.Location = new System.Drawing.Point(130, 23);
+            this.NextDirBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.NextDirBtn.Name = "NextDirBtn";
+            this.NextDirBtn.Size = new System.Drawing.Size(82, 22);
+            this.NextDirBtn.TabIndex = 8;
+            this.NextDirBtn.Text = "Forward";
+            this.NextDirBtn.UseVisualStyleBackColor = true;
+            this.NextDirBtn.Click += new System.EventHandler(this.NextDirBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NextDirBtn);
+            this.Controls.Add(this.PreviousDirBtn);
             this.Controls.Add(this.CurrentDir);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.FilesTable);
@@ -118,11 +154,14 @@
         }
 
         #endregion
-        private DataGridView FilesTable;
         private Label label1;
         private Label CurrentDir;
+        private Button PreviousDirBtn;
+        private DataGridView FilesTable;
         private DataGridViewTextBoxColumn DirName;
-        private DataGridViewTextBoxColumn FileType;
-        private DataGridViewTextBoxColumn DirSize;
+        private DataGridViewTextBoxColumn DirType;
+        private DataGridViewTextBoxColumn FileSize;
+        private DataGridViewTextBoxColumn DateCreated;
+        private Button NextDirBtn;
     }
 }
--- MyFileExplorer/Form1.cs
+++ MyFileExplorer/Form1.cs
@@ -1,6 +1,10 @@
 using Microsoft.VisualBasic.ApplicationServices;
 using System;
+using System.Collections;
 using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
 
 namespace MyFileExplorer
 {
@@ -12,9 +16,9 @@ namespace MyFileExplorer
             InitializeComponent();
         }
 
-        String GetFileSize(Double fileSize)
+        string GetFileSize(double fileSize)
         {
-            String[] fileSizes = { "B", "KB", "MB", "GB, TB" };
+            string[] fileSizes = { "B", "KB", "MB", "GB, TB" };
             int fileSizeIndex = 0;
 
             /*
@@ -32,41 +36,79 @@ namespace MyFileExplorer
             return fileSize + " " + fileSizes[fileSizeIndex];
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+
+        /*
+         * initial directory to load
+         * it's placed here so it's accessible across all functions
+         * and it's loaded once so the it's value can be changed later
+         */
+        string currentDir = "C:\\";
+
+        //used for loading the previous directory
+        string? directory = "";
+
+
+        //directory stack to keep track of visted directories
+        string[] dirStack = new string[65536];
+        int top = 0;
+
+        //loads the files and folders in a given directory into the files table
+        private void LoadDirectory(string path)
         {
-            //dynamically set the current directory to the user's desktop
-            string username = Environment.UserName;
-            string currentDir = @"C:\Users\" + username + "\\Desktop";
+            FilesTable.Rows.Clear();
+
+            currentDir += path;
             CurrentDir.Text = currentDir;
 
-            //files and folders in directory
-            string[] folders = Directory.GetDirectories(currentDir);
-            string[] files = Directory.GetFiles(currentDir);
-
-            double fileSize;
-
-            //read all folders from given directory
-            for (int i = 0; i < folders.Length; i++)
+            if (top < dirStack.Length)
             {
-                FileInfo folder = new FileInfo(folders[i]);
-                FilesTable.Rows.Add(folder.Name, "Folder");
+                top++;
+                dirStack[top] = currentDir;
             }
-            //read all files from given directory
-            for (int i = 0; i < files.Length; i++)
+
+            try
             {
-                FileInfo file = new FileInfo(files[i]);
-                fileSize = file.Length;
-                FilesTable.Rows.Add(file.Name, "File", GetFileSize(fileSize));
+                //get files and folders in the current directory
+                string[] folders = Directory.GetDirectories(currentDir);
+                string[] files = Directory.GetFiles(currentDir);
+
+                double fileSize;
+
+                //read all folders from given directory
+                for (int i = 0; i < folders.Length; i++)
+                {
+                    FileInfo folder = new FileInfo(folders[i]);
+                    FilesTable.Rows.Add(folder.Name, "Folder", "", folder.CreationTime);
+                }
+                //read all files from given directory
+                for (int i = 0; i < files.Length; i++)
+                {
+                    FileInfo file = new FileInfo(files[i]);
+                    fileSize = file.Length;
+                    FilesTable.Rows.Add(file.Name, "File", GetFileSize(fileSize), file.CreationTime);
+                }
+            }
+            catch
+            {
+                string message = "Couldn't load this folder: " + currentDir;
+                MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        //variables to be used below
-        DataGridViewRow? row;
-        String? data;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dirStack[top] = currentDir;
+            NextDirBtn.Enabled = false;
+            LoadDirectory("");
+        }
 
-        //called every time the user double clicks on a row in the tables
+        //called every time the user double clicks on a row (file/folder) in the table
         private void FilesTable_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
         {
+
+            //variables to be used below
+            DataGridViewRow? row;
+
             /*
              * checks if the index of the selected row
              * is within the range of 0 to the amount of processes
@@ -83,12 +125,109 @@ namespace MyFileExplorer
                  * so then the program can read the contents of that row
                  */
 
-                if (row.Cells[0].Value != null)
+                if (row.Cells[0].Value != null && row.Cells[1].Value != null)
                 {
-                    data = row.Cells[0].Value.ToString();
-                    CurrentDir.Text += data + "\\";
+                    if (row.Cells[1].Value.ToString() == "Folder")
+                    {
+                        //read next directory's name
+                        directory = "\\"  + row.Cells[0].Value.ToString();
+                        CurrentDir.Text += directory;
+
+                        //load the next directory
+                        LoadDirectory(directory);
+                    }
                 }
             }
+        }
+
+        //loads the files and folders in a given directory into the files table
+        private void LoadPreviousDirectory(string? path, string direction)
+        {
+            FilesTable.Rows.Clear();
+
+            if (direction == "Up")
+            {
+                if (top < dirStack.Length)
+                {
+                    top++;
+
+                    //disable the forward button
+                    if (String.IsNullOrEmpty(dirStack[top]))
+                    {
+                        NextDirBtn.Enabled = false;
+                        top--;
+                    } else
+                    {
+                        // set the current directory to the top of the dirStack if it's NOT empty
+                        currentDir = dirStack[top];
+                        NextDirBtn.Enabled = true;
+                    }
+                }
+            } else 
+            {
+                //go back to previously visited directory
+                if (top > 0) {
+                    top--;
+                    currentDir = dirStack[top];
+                    NextDirBtn.Enabled = true;
+                }
+            }
+
+            try
+            {
+                //get the previous directory to load
+                //currentDir = currentDir.Remove(stringToRemoveIndex);
+                
+                CurrentDir.Text = currentDir;
+
+                //get files and folders in the current directory
+                string[] folders = Directory.GetDirectories(currentDir);
+                string[] files = Directory.GetFiles(currentDir);
+
+                double fileSize;
+
+                //read all folders from given directory
+                for (int i = 0; i < folders.Length; i++)
+                {
+                    FileInfo folder = new FileInfo(folders[i]);
+                    FilesTable.Rows.Add(folder.Name, "Folder", "", folder.CreationTime);
+                }
+                //read all files from given directory
+                for (int i = 0; i < files.Length; i++)
+                {
+                    FileInfo file = new FileInfo(files[i]);
+                    fileSize = file.Length;
+                    FilesTable.Rows.Add(file.Name, "File", GetFileSize(fileSize), file.CreationTime);
+                }
+            }
+            catch
+            {
+                /*
+                 * when removing the \\ character makes the currentDir C:
+                 * set currentDir to C:\\ and load that directory instead
+                 */
+                if (currentDir == "C:")
+                {
+                    currentDir = "C:\\";
+                    CurrentDir.Text = currentDir;
+                    LoadDirectory("");
+                } else
+                {
+                    string message = "Couldn't load this folder: " + currentDir;
+                    MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
+        }
+
+        private void PreviousDirBtn_Click(object sender, EventArgs e)
+        {
+            LoadPreviousDirectory(directory, "Down");
+        }
+
+        private void NextDirBtn_Click(object sender, EventArgs e)
+        {
+            LoadPreviousDirectory(directory, "Up");
         }
     }
 }
